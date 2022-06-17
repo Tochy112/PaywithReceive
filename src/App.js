48aa7c3d7114c5663f2d59d11a290605b1bd058d
Ukwuoma Tochukwu <ukwuomatochi112@gmail.com>
2022-06-17 15:05:26 +0100

    designed signup-options page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Option from './pages/sign-option-page/signOption';
 
 function App() {
   return (
     <div className="App">
-
+      <Option />
     </div>
   );
 }
---
+++ src/pages/sign-option-page/signOption.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './signOption.css'
+import logo from '../../images/brand.png'
+
+function signOption() {
+  return (
+    <div className='option-wrapper'>
+
+      <div className='logo-div'>
+          <img src={logo} alt="brand-logo"  />
+      </div>
+      <div className='option-div'>
+        <div className='btn-div'>
+          <button className='btn1'>sign Up</button>
+          <p>Already a member?</p>
+          <button className='btn2'>Log In</button>
+        </div>
+
+         <div className='terms-div'>
+            <input type="checkbox" name="check" id="check" />
+            <p>
+              By signing up, I confirmed that I have read and 
+              agreed to Recieve's Privacy Policy, Cookies and Terms.
+            </p>
+         </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default signOption
