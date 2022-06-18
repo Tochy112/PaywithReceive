2a3a90c008eefe46d3312f9f543fe46cfceffcbc
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2022-06-18 04:03:43 +0100

    update

---
+++ src/Pages/Onboarding/Onboarding.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Onboarding.css'
+
+
+
+const Onboarding = () => {
+  return (
+   
+        <div class="onboard">
+      <div className='dots'>
+        <div class="dot active"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+      </div>
+      <div class="steps">
+        <div class='step'>
+          <div class='gradient'></div>
+          <p>Skip long queues, send and receive money anywhere</p>
+        </div>
+      </div>
+        </div>
+     
+
+   
+  )
+}
+
+export default Onboarding
