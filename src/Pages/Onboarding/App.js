8edf41cc5efc1632f9169a3e77232b4485552621
Imejuart1 <101446446+Imejuart1@users.noreply.github.com>
2022-06-19 10:37:36 +0100

    how the app.js looks like on my end

--- src/Pages/Onboarding/App.js
+++ src/Pages/Onboarding/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
 import Onboarding from './android/app/src/screens/Onboarding';
 import Home from './android/app/src/screens/Home';
 
