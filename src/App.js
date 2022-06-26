9d2669e9f37487ddb0de8b04d5e6b7daaaf1e75f
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2022-06-26 14:27:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import {
-  BrowserRouter,
-  Routes,
+  BrowserRouter as Router,
+  Switch,
   Route,
+  Redirect,
+  Routes,
+  Navigate,
 } from "react-router-dom";
+  import Yome from './Pages/Onboarding/Yome.js';
 import Onboarding0 from './Pages/Onboarding/Onboarding0';
 import Onboarding from './Pages/Onboarding/Onboarding';
 import SignupOption from './Pages/Signup Option/SignupOption';
@@ -12,21 +16,27 @@ import Login from './Pages/Login/Login';
 import Footer from './Components/Footer/Footer';
 import SignupDetails from './Pages/Signup Details/SignupDetails';
 
+
 function App() {
+  
+  
   return (
     <div className="App">
       
-      <BrowserRouter>
+      <Router>
         <Routes>
       
-          <Route path="/" element={<Onboarding />} />
+          <Route exact path="/" element={<Onboarding />} />
+          <Route path="/onboarding0" element={<Onboarding0 />} />
+          <Route path="/yome" element={<Yome />} />
           <Route path="/signupLogin" element={<SignupOption />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signupDetails" element={<SignupDetails />} />
           <Route path="/login" element={<Login />} />
+
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Onboarding/Onboarding.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import meju from "./images/ON.png"
+import './Onboarding.css'
+const Onboarding = () => {
+ 
+  return (
+     <article >
+ 
+     <div className='pics'> 
+    <Link to="/onboarding0">
+     <img  src={meju} alt="image"/> <h1 className='pext' >tap anywhere</h1></Link>
+    
+     </div>
+   
+      
+       
+    </article>
+
+  )
+}
+
+export default Onboarding
