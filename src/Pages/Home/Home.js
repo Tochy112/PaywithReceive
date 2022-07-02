107036517ebb433ee92f1236458027d672b8ee61
Ikwuegbuenyi Augustus <mikwuegbuenyi@gmail.com>
2022-07-02 19:49:51 +0100

    still workimg

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Home.css'
 import Cash from "../../Assets/Images/cash.png"
-import "./Home.css"
+import first from "../../Assets/Images/1.png"
+import second from "../../Assets/Images/2.png"
+import third from "../../Assets/Images/3.png"
+import fourth from "../../Assets/Images/4.png"
+import fifth from "../../Assets/Images/5.png"
+import sixth from "../../Assets/Images/6.png"
 const Home = () => {
   return (
     <body>
-    <section className="overall">
+    <section className="first-section">
     <div className="general">
     
         <div  className="alltext">
@@ -32,6 +37,51 @@ const Home = () => {
         </div>
            </div>
            </section>
+
+        {/*SECOND SECTION*/}
+
+        <section className="container second-section">
+            <h2> Why use Receive account for your Transaction </h2>
+             <div className="reasons">
+              <article className= "offers">
+                <div className="icons">
+                  <img src= {first} alt="first icon"/>
+                   </div>
+                  <h3>Fast Payments & Transactions</h3>
+               </article>
+                 <article className= "offers">
+                <div className="icons2">
+                  <img src= {second} alt="second icon"/>
+                   </div>
+                  <h3>Cryptocurrency payment</h3>
+               </article>
+                 <article className= "offers">
+                <div className="icons3">
+                  <img src= {third} alt="first icon"/>
+                   </div>
+                  <h3>Multiple Payment methods</h3>
+               </article>
+                 <article className= "offers">
+                <div className="icons4">
+                  <img src= {fourth} alt="first icon"/>
+                   </div>
+                  <h3>Privacy security</h3>
+               </article>
+                 <article className= "offers">
+                <div className="icons5">
+                  <img src= {fifth} alt="first icon"/>
+                   </div>
+                  <h3>Reliable Customer support</h3>
+               </article>
+                 <article className= "offers">
+                <div className="icons6">
+                  <img src= {sixth} alt="first icon"/>
+                   </div>
+                  <h3>API for Developers</h3>
+               </article>
+
+             </div>
+        </section>
     </body>
   )
 }
